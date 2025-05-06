0d96b21f9621c16f344f300b855a523a38d75734
chatwrapped <chat.wrapped.io@gmail.com>
2025-05-06 20:49:58 +0200

    prova

--- bk.ts
+++ bk.ts
@@ -11,7 +11,7 @@ export interface ChatAnalysis {
         [username: string]: {
             messageCount: number;
             wordCount: number;
-            emojiCount: number;
+            emojiCount: number
         };
     };
 }
